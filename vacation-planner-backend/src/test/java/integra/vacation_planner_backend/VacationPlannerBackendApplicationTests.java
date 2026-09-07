33a264564d47ce4c01c2d990ac462a9c1e6af0bd
Octavian Custura <octavian.custura@cloudflight.io>
2026-09-07 16:30:46 +0300

    Create Backend

---
+++ vacation-planner-backend/src/test/java/integra/vacation_planner_backend/VacationPlannerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package integra.vacation_planner_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationPlannerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
