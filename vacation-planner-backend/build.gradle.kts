6cdafb4937c2bc46f25e417c022f0e61d5d12908
Andreea Ploscar <Andreea.Ploscar@cloudflight.io>
2026-09-17 10:05:32 +0300

    add missing dep

--- vacation-planner-backend/build.gradle.kts
+++ vacation-planner-backend/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-webmvc")
-	implementation("org.flywaydb:flyway-core:11.0.0")
+	implementation("org.springframework.boot:spring-boot-starter-flyway")
 	implementation("org.flywaydb:flyway-database-postgresql")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
