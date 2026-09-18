11bdb3d56530f086fa19b61e92edd057201d7b55
Octavian Custura <octavian.custura@cloudflight.io>
2026-09-18 17:40:47 +0300

    fix(requests): Filter Sprint Security Requests to Permit All

---
+++ vacation-planner-backend/src/main/java/integra/vacation_planner_backend/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package integra.vacation_planner_backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().permitAll()
+                )
+                .csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
+}
